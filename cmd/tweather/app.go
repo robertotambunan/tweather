891ffdb0aae8be760f5551ef2aae7459d1db8a94
robertotambunan <robertotambunan14@gmail.com>
2021-04-18 00:47:47 +0700

    adjust git ignore

---
+++ cmd/tweather/app.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/dghubble/go-twitter/twitter"
+	"github.com/dghubble/oauth1"
+	"github.com/sirupsen/logrus"
+
+	"github.com/spf13/viper"
+
+	"github.com/robertotambunan/tweather/deliveries"
+	bmkgHTTPR "github.com/robertotambunan/tweather/repositories/bmkg/http"
+	twHTTPR "github.com/robertotambunan/tweather/repositories/twitter/http"
+	wUC "github.com/robertotambunan/tweather/usecases/weather"
+)
+
+func main() {
+	// load configration
+	filePath := "config.toml"
+	viper.SetConfigType("toml")
+	viper.SetConfigFile(filePath)
+	err := viper.ReadInConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+	logrus.Info("Using Config file: ", viper.ConfigFileUsed())
+
+	if viper.GetString("credential.consumer.key") == "" || viper.GetString("credential.consumer.secret") == "" ||
+		viper.GetString("credential.access.token") == "" || viper.GetString("credential.access.secret") == "" {
+		log.Fatal("Invalid configuration")
+	}
+
+	config := oauth1.NewConfig(viper.GetString("credential.consumer.key"), viper.GetString("credential.consumer.secret"))
+	token := oauth1.NewToken(viper.GetString("credential.access.token"), viper.GetString("credential.access.secret"))
+	// OAuth1 http.Client will automatically authorize Requests
+	httpClient := config.Client(oauth1.NoContext, token)
+
+	// Twitter client
+	twClient := twitter.NewClient(httpClient)
+
+	// repositories
+	bmkgHTTPRepo := bmkgHTTPR.NewBMKGHTTPRepo(&http.Client{})
+	twitterHTTPRepo := twHTTPR.NewTwitterHTTP(twClient)
+
+	// usecases
+	weatherUC := wUC.NewWeatherUC(twitterHTTPRepo, bmkgHTTPRepo)
+
+	// deliveres
+	cronDelivery := deliveries.NewCron(weatherUC)
+
+	cronDelivery.ActivateWheater()
+
+}
